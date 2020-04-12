6be73414ccaad1354eda2c6b07a0e64e858ef8db
Alisa Chang <laptite@gmail.com>
2020-04-11 23:23:33 -0700

    Add association between User and Item models

--- db/schema.rb
+++ db/schema.rb
@@ -10,13 +10,14 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2020_04_12_060602) do
+ActiveRecord::Schema.define(version: 2020_04_12_062032) do
 
   create_table "items", force: :cascade do |t|
     t.string "title"
     t.text "description"
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
+    t.integer "user_id"
   end
 
   create_table "users", force: :cascade do |t|
